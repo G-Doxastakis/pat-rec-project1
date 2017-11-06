bc11b80d6dbddddc90460b426b806a444ad97f0a
George Metsis <geoptw@gmail.com>
2017-11-06 16:13:35 +0200

    Fixes #3

--- UCIdata-exercise1/pimadiabetes_simple_main.m
+++ UCIdata-exercise1/pimadiabetes_simple_main.m
@@ -12,21 +12,21 @@ Y = table2array(pimaindiansdiabetes(:,5));
 rng(10); % For reproducibility
 Mdl = fitcknn(X,Y);
 
-correlation = zeros(upToK,2);
+g = zeros(upToK,2);
 for K = 1:upToK
     Mdl.NumNeighbors = K;
     loss = resubLoss(Mdl);
     CVMdl = crossval(Mdl,'KFold',10);
     kloss = kfoldLoss(CVMdl);
-    correlation(K,1) = K;
-    correlation(K,2) = kloss;
+    g(K,1) = K;
+    g(K,2) = kloss;
 end
-plot(correlation(:,1), correlation(:,2));
+plot(g(:,1), g(:,2));
 xlabel('K');
 ylabel('error');
 
-[minError, ind] = min(correlation(:,2));
-bestK = correlation(ind,1);
+[minError, ind] = min(g(:,2));
+bestK = g(ind,1);
 txt2 = strcat(strcat('best K = ',num2str(bestK)), ' \rightarrow');
 text(bestK,minError,txt2,'Color','red','HorizontalAlignment','right');
 
