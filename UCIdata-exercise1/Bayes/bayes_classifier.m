6234683fa44ab7ce462204b20cfefa78ac4e09dd
George Metsis <geoptw@gmail.com>
2017-12-14 01:25:32 +0200

    Bayes Classifier documentation and optimization

--- UCIdata-exercise1/Bayes/bayes_classifier.m
+++ UCIdata-exercise1/Bayes/bayes_classifier.m
@@ -1,8 +1,9 @@
-%Bayes classifier
-%Assumes last column of input TRAIN and TEST matrices is the class
-%set covarianceMode to 1 for common diagonal covariance matrix
-%set covarianceMode to 2 for common covariance matrix
-%set covarianceMode to 3 for a seperate covariance matrix for each class
+% Bayes Classifier
+% Assumes last column of input TRAIN and TEST matrices is the class
+% 
+% set covarianceMode to 1 for common diagonal covariance matrix
+% set covarianceMode to 2 for common covariance matrix
+% set covarianceMode to 3 for a seperate covariance matrix for each class
 
 function testval = bayes_classifier(XTRAIN, XTEST, covarianceMode)
 if nargin < 3
@@ -14,36 +15,38 @@ fprintf('Fitting Bayes Classifier :\n');
 trainDataSize  = size(XTRAIN,1);
 testDataSize  = size(XTEST,1);
 
-%Separate all the training samples into separate classes
+% Separate all the training samples into separate classes
 [C,~,idx] = unique(XTRAIN(:,end));
 classes = accumarray(idx,1:trainDataSize,[],@(r){XTRAIN(r,1:end-1)});
 
-%Generate the summary map from all the classes 
+% Generate the summary map from all the classes 
 classMap = containers.Map(C, classes);
 
 covMap = containers.Map('KeyType','int32','ValueType','any');
 meanMap = containers.Map('KeyType','int32','ValueType','any');
 
+% -------------------Calculate covariance and mean-------------------------
 switch covarianceMode
-    case 1
-        %Calculate the common diagonal covariance and mean of all the features for all the
-        %classes
+    case 1 % Use a common DIAGONAL covariance regardless of class
         fprintf('using COMMON DIAGONAL covariance matrix for all classes :\n');
         
+        % First gather all features
         allClassesFeatures = [];
         for i = 1:size(C,1)
             c = C(i);
             allClassesFeatures = [allClassesFeatures; classMap(c)];
         end
         
+        % Calculate one covariance matrix regardless of class and multiply
+        % it by the diagonal matrix
+        covariance = var(allClassesFeatures(:)) * eye (size(allClassesFeatures,2));
+        
         for i = 1:size(C,1)
             c = C(i);
-            covMap(c) = cov(allClassesFeatures(:)) * eye (size(allClassesFeatures,2));
-            meanMap(c) = mean(allClassesFeatures,1);
+            covMap(c) = covariance; % Use the same covariance matrix for all  classes
+            meanMap(c) = mean(classMap(c),1);
         end
-    case 2
-        %Calculate the common covariance and mean of all the features for all the
-        %classes
+    case 2 % Use a common covariance matrix regardless of class
         fprintf('using COMMON covariance matrix for all classes :\n');
         
         allClassesFeatures = [];
@@ -51,70 +54,128 @@ switch covarianceMode
             c = C(i);
             allClassesFeatures = [allClassesFeatures; classMap(c)];
         end
+        
+        % Calculate one covariance matrix regardless of class
+        covariance = cov(allClassesFeatures);
+        
         for i = 1:size(C,1)
             c = C(i);
-            covMap(c) = cov(allClassesFeatures);
-            meanMap(c) = mean(allClassesFeatures,1);
+            covMap(c) = covariance;
+            meanMap(c) = mean(classMap(c),1);
         end
     case 3 
-        %Calculate the covariance and mean of all the features for all the
-        %classes seperately
+        % Use seperate covariance matrices for each class
         fprintf('using SEPERATE covariance matrix for each class :\n');
 
         for i = 1:size(C,1)
             c = C(i);
             if size(classMap(c),1) ~= 0
-                covMap(c) = cov(classMap(c));
+                covMap(c) = cov(classMap(c)); % Calculate the covariance matrix of THIS class
                 meanMap(c) = mean(classMap(c),1);
             end
         end
 end
 
-%-------------Find the accuracy of training and testing data------
+% -------------Find the accuracy of training and testing data--------------
 
-%Training
-calculatedY = [];
-for trainIndex=1:trainDataSize
-    prob = [];
-    for i = 1:size(C,1)
-        c = C(i);
-        if size(classMap(c),1) ~= 0 
-            condProb = mvnpdf(XTRAIN(trainIndex,1:end-1), meanMap(c), covMap(c));
-            condProb = prod(condProb);
-            priorProb = size(classMap(c),1)/trainDataSize;
-            prob = cat(2,prob,condProb*priorProb);
-        else
-            prob = cat(2,prob,0);
-        end
+% First get the a priori probabilities for each class
+priors = []; % Initialise some space to store the priors
+ for i = 1:size(C,1) % Loop  over all classes
+     
+        c = C(i); % Get the current class
+        
+        % Get the a priori probability of this class
+        priorProb = size(classMap(c),1)/trainDataSize;
+        
+        % Store it for later use
+        priors = cat(2, priors, priorProb); 
+ end
+
+% Training ----------------------------------------------------------------
+
+% Initialise some space to store the estimated class for each x
+calculatedY = []; 
+
+% Do the estimate! 
+for trainIndex=1:trainDataSize % Loop  over all x in the train set
+    
+    prob = []; % Initialise some space to store the resulting probabilities
+    
+    % Calculate the a posteriori probabilities for all classes 
+    % for current x
+    for i = 1:size(C,1) % Loop  over all classes
+        
+        c = C(i); % Get the current class
+        
+        % Get the current class' a posteriori probability given current x
+        % using mean and covariance for this class.
+        % (Note: depending on the covarianceMode chosen, common and  diagonal, 
+        % common or seperate covariance matrices might be used. See above.)
+        condProb = mvnpdf(XTRAIN(trainIndex,1:end-1), meanMap(c), covMap(c));
+        
+        % Put the a posteriori and a priori probabilities of current class
+        % for current x side by side for 
+        % later comparison
+        prob = cat(2,prob,condProb*priors(i)); 
     end
+    
+    % Get the greatest a posteriori probability (estimate of which class the
+    % x falls under)
     [maxval,argmax] = max(prob);
+    
+    % Store the estimated class for later use
     calculatedY = [calculatedY;argmax];
 end
 
+% Now count how many estimates were right
 accuratePredictions = (calculatedY == XTRAIN(:,end));
+% Get the accuracy percentage
 trainingAccuracy = sum(accuratePredictions)*100/size(accuratePredictions,1);
 fprintf('Training Accuracy = %4.2f\n',trainingAccuracy);
+
     
-%Testing
+%Testing ------------------------------------------------------------------
+
+% Initialise some space to store the estimated class for each x
 calculatedY = [];
-for testIndex=1:testDataSize
-    prob = [];
-    for i = 1:size(C,1)
-        c = C(i);
-        if size(classMap(c),1) ~= 0 
-            condProb = mvnpdf(XTEST(testIndex,1:end-1), meanMap(c), covMap(c));
-            condProb = prod(condProb);
-            priorProb = size(classMap(c),1)/trainDataSize;
-            prob = cat(2,prob,condProb*priorProb);
-        else
-            prob = cat(2,prob,0);
-        end
+
+% Do the estimate! 
+for testIndex=1:testDataSize % Loop  over all x in the train set
+    
+    prob = []; % Initialise some space to store the resulting probabilities
+    
+    % Calculate the a posteriori probabilities for all classes 
+    % for current x
+    for i = 1:size(C,1) % Loop  over all classes
+        
+        c = C(i); % Get the current class
+        
+        % Get the current class' a posteriori probability given current x
+        % using mean and covariance for this class.
+        % (Note: depending on the covarianceMode chosen, common and  diagonal, 
+        % common or seperate covariance matrices might be used. See above.)
+        condProb = mvnpdf(XTEST(testIndex,1:end-1), meanMap(c), covMap(c));
+        
+        % Put the a posteriori and a priori probabilities of current class
+        % for current x side by side for 
+        % later comparison
+        prob = cat(2, prob, condProb*priors(i));
     end
+    
+    % Get the greatest a posteriori probability (estimate of which class the
+    % x falls under)
     [maxval,argmax] = max(prob);
+    
+    % Store the estimated class for later use
     calculatedY = [calculatedY;argmax];
 end
 
+% Now count how many estimates were right
 accuratePredictions = (calculatedY == XTEST(:,end));
+% Get the accuracy percentage
 testAccuracy = sum(accuratePredictions)*100/size(accuratePredictions,1);
 fprintf('Test Accuracy = %4.2f\n\n',testAccuracy);
+
+
+% return the accuracy percentage to the caller
 testval = testAccuracy;
--- UCIdata-exercise1/Bayes/gaussian_bayes.m
+++ UCIdata-exercise1/Bayes/gaussian_bayes.m
@@ -6,19 +6,45 @@ tic
 rng(8); % For reproducibility
 load('../datasets.mat');
 
-%Randomize dataset
+% Randomize dataset
 dataSetSize = size(pimaindiansdiabetes,1);
 data_rand = table2array(pimaindiansdiabetes(randperm(dataSetSize),:));
 data_rand(:,end) = data_rand(:,end) + 1; %Shift class one value up to help with later calculations
 
+accuracies = [];
 
-%set mode to 1 for common diagonal covariance matrix
-%set mode to 2 for common covariance matrix
-%set mode to 3 for a seperate covariance matrix for each class
-mode = 3;
-classifier = @(XTRAIN, XTEST)(bayes_classifier(XTRAIN, XTEST, mode));
-%10fold cross-validate
-overallAccuracy = mean(crossval(classifier, data_rand));
-fprintf('Bayes Overall Accuracy = %4.2f\n',overallAccuracy);
+% Calculate accuracy rates for all different covariance modes using 
+% 10fold cross-validation method
+for i = 1:3 % Loop over all covariance modes
+    
+    % mode 1 for common diagonal covariance matrix
+    % mode 2 for common covariance matrix
+    % mode 3 for a seperate covariance matrix for each class
+    covarianceMode = i;
+    
+    % Wrap the classifier function inside another so we can pass the mode as a
+    % parameter and keep matlab happy.
+    % That is because matlab's crossval function takes as first
+    % parameter a function who's signature is as follows:
+    % @(XTRAIN, XTEST)(success_rate)
+    % The first being the train data set and the seconf the test data set.
+    % Our bayes_classifier accepts a third parameter as well.
+    classifier = @(XTRAIN, XTEST)(bayes_classifier(XTRAIN, XTEST, covarianceMode));
 
+    % 10fold cross-validate
+    overallAccuracy = mean(crossval(classifier, data_rand));
+    fprintf('Bayes Overall Accuracy = %4.2f\n',overallAccuracy);
+    accuracies = [accuracies; overallAccuracy]; 
+end
+figure;
+x = categorical({'common diagonal covariance matrix','common covariance matrix','seperate covariance matrix for each class'});
+bar(x, accuracies);
+title('Comparison of classifier accuracy with different covariance modes');
+xlabel('Covariance Mode:'); % x-axis label
+ylabel('Accuracy %:'); % y-axis label
+for i=1:3
+    text(x(i),accuracies(i),num2str(accuracies(i),'%0.2f'),...
+               'HorizontalAlignment','center',...
+               'VerticalAlignment','bottom')
+end
 toc
