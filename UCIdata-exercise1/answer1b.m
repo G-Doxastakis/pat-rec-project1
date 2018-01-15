a265583f16f3e3aafefffb0850c6410cac74d0c4
George <geoptw@gmail.com>
2018-01-16 00:37:44 +0200

    Update answer 1b

--- UCIdata-exercise1/answer1b.m
+++ UCIdata-exercise1/answer1b.m
@@ -9,7 +9,7 @@ load('datasets.mat');
 %Randomize dataset
 dataSetSize = size(pimaindiansdiabetes,1);
 data_rand = table2array(pimaindiansdiabetes(randperm(dataSetSize),:)); 
-data_rand(:,end) = data_rand(:,end) + 1; %Shift class one value up to help with later calculations
+data_rand(:,end) = data_rand(:,end) +1; %Shift class one value up to help with later calculations
 
 %------Train the gaussian bayes classifier--------------
 
